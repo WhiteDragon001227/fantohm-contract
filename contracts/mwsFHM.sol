4b0e6363bbf0203c02df620f94050e63c06e01b0
Chinu Dev <admin@chubbyinu.com>
2021-11-30 16:13:28 +0100

    wrapped tokens

--- contracts/mwsFHM.sol
+++ contracts/mwsFHM.sol
@@ -745,97 +745,97 @@ interface IStaking {
     function index() external view returns ( uint );
 }
 
-contract wFHM is ERC20 {
+contract mwsFHM is ERC20 {
     using SafeERC20 for ERC20;
     using Address for address;
     using SafeMath for uint;
 
     address public immutable staking;
-    address public immutable OHM;
-    address public immutable sOHM;
+    address public immutable FHM;
+    address public immutable sFHM;
 
-    constructor( address _staking, address _OHM, address _sOHM ) ERC20( 'Wrapped sOHM', 'wsOHM' ) {
+    constructor( address _staking, address _FHM, address _sFHM) ERC20( 'Moonriver Wrapped sFHM', 'mwsFHM' ) {
         require( _staking != address(0) );
         staking = _staking;
-        require( _OHM != address(0) );
-        OHM = _OHM;
-        require( _sOHM != address(0) );
-        sOHM = _sOHM;
+        require( _FHM != address(0) );
+        FHM = _FHM;
+        require( _sFHM != address(0) );
+        sFHM = _sFHM;
     }
 
     /**
-    @notice stakes OHM and wraps sOHM
+    @notice stakes FHM and wraps sFHM
     @param _amount uint
     @return uint
- */
-    function wrapFromOHM( uint _amount ) external returns ( uint ) {
-        IERC20( OHM ).transferFrom( msg.sender, address(this), _amount );
+    */
+    function wrapFromFHM( uint _amount ) external returns ( uint ) {
+        IERC20( FHM ).transferFrom( msg.sender, address(this), _amount );
 
-        IERC20( OHM ).approve( staking, _amount ); // stake OHM for sOHM
+        IERC20( FHM ).approve( staking, _amount ); // stake FHM for sFHM
         IStaking( staking ).stake( _amount, address(this) );
 
-        uint value = wOHMValue( _amount );
+        uint value = wsFHMValue( _amount );
         _mint( msg.sender, value );
         return value;
     }
 
     /**
-        @notice unwrap sOHM and unstake OHM
+        @notice unwrap sFHM and unstake FHM
         @param _amount uint
         @return uint
      */
-    function unwrapToOHM( uint _amount ) external returns ( uint ) {
+    function unwrapToFHM( uint _amount ) external returns ( uint ) {
         _burn( msg.sender, _amount );
 
-        uint value = sOHMValue( _amount );
-        IERC20( sOHM ).approve( staking, value ); // unstake sOHM for OHM
+        uint value = sFHMValue( _amount );
+        IERC20( sFHM ).approve( staking, value ); // unstake sFHM for FHM
         IStaking( staking ).unstake( value, address(this) );
 
-        IERC20( OHM ).transfer( msg.sender, value );
+        IERC20( FHM ).transfer( msg.sender, value );
         return value;
     }
 
     /**
-        @notice wrap sOHM
+        @notice wrap sFHM
         @param _amount uint
         @return uint
      */
-    function wrapFromsOHM( uint _amount ) external returns ( uint ) {
-        IERC20( sOHM ).transferFrom( msg.sender, address(this), _amount );
+    function wrapFromsFHM( uint _amount ) external returns ( uint ) {
+        IERC20( sFHM ).transferFrom( msg.sender, address(this), _amount );
 
-        uint value = wOHMValue( _amount );
+        uint value = wsFHMValue( _amount );
         _mint( msg.sender, value );
         return value;
     }
 
     /**
-        @notice unwrap sOHM
+        @notice unwrap sFHM
         @param _amount uint
         @return uint
      */
-    function unwrapTosOHM( uint _amount ) external returns ( uint ) {
+    function unwrapTosFHM( uint _amount ) external returns ( uint ) {
         _burn( msg.sender, _amount );
 
-        uint value = sOHMValue( _amount );
-        IERC20( sOHM ).transfer( msg.sender, value );
+        uint value = sFHMValue( _amount );
+        IERC20( sFHM ).transfer( msg.sender, value );
         return value;
     }
 
     /**
-        @notice converts wOHM amount to sOHM
+        @notice converts wsFHM amount to sFHM
         @param _amount uint
         @return uint
      */
-    function sOHMValue( uint _amount ) public view returns ( uint ) {
+    function sFHMValue( uint _amount ) public view returns ( uint ) {
         return _amount.mul( IStaking( staking ).index() ).div( 10 ** decimals() );
     }
 
     /**
-        @notice converts sOHM amount to wOHM
+        @notice converts sFHM amount to wsFHM
         @param _amount uint
         @return uint
      */
-    function wOHMValue( uint _amount ) public view returns ( uint ) {
+    function wsFHMValue( uint _amount ) public view returns ( uint ) {
         return _amount.mul( 10 ** decimals() ).div( IStaking( staking ).index() );
     }
 
