fa82e34c0f854ee238cd9ef046df0ddfa04a8709
pwntr0n <pwntr0n@fantohm.com>
2022-02-27 00:03:29 +0100

    balancer add tokens todo

--- contracts/masterChef/MasterChef.sol
+++ contracts/masterChef/MasterChef.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.7.5;
+pragma abicoder v2;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -8,7 +9,39 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-import "../FantohmERC20.sol";
+interface ITreasury {
+    function deposit( uint _amount, address _token, uint _profit ) external returns ( uint send_ );
+    function valueOf( address _token, uint _amount ) external view returns ( uint value_ );
+    function mintRewards( address _recipient, uint _amount ) external;
+}
+
+interface IFHUDMinter {
+    function getMarketPrice() external view returns (uint);
+}
+
+interface IMintable {
+    function mint(address to, uint256 amount) external;
+}
+
+interface IBurnable {
+    function burn(uint256 amount) external;
+}
+
+struct JoinPoolRequest {
+    address[] assets;
+    uint256[] maxAmountsIn;
+    bytes userData;
+    bool fromInternalBalance;
+}
+
+interface IVault {
+    function joinPool(
+        bytes32 poolId,
+        address sender,
+        address recipient,
+        JoinPoolRequest memory request
+    ) external payable;
+}
 
 // MasterChef is the master of FHM. He can make FHM and he is a fair guy.
 //
@@ -18,21 +51,21 @@ import "../FantohmERC20.sol";
 //
 // Have fun reading it. Hopefully it's bug-free. God bless.
 contract MasterChefV2 is Ownable, ReentrancyGuard {
-    using SafeMath for uint256;
+    using SafeMath for uint;
     using SafeERC20 for IERC20;
 
     // Info of each user.
     struct UserInfo {
-        uint256 amount;         // How many LP tokens the user has provided.
-        uint256 rewardDebt;     // Reward debt. See explanation below.
+        uint amount;         // How many LP tokens the user has provided.
+        uint rewardDebt;     // Reward debt. See explanation below.
         //
         // We do some fancy math here. Basically, any point in time, the amount of FHMs
         // entitled to a user but is pending to be distributed is:
         //
-        //   pending reward = (user.amount * pool.accLqdrPerShare) - user.rewardDebt
+        //   pending reward = (user.amount * pool.accFhmPerShare) - user.rewardDebt
         //
         // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-        //   1. The pool's `accLqdrPerShare` (and `lastRewardBlock`) gets updated.
+        //   1. The pool's `accFhmPerShare` (and `lastRewardBlock`) gets updated.
         //   2. User receives the pending reward sent to his/her address.
         //   3. User's `amount` gets updated.
         //   4. User's `rewardDebt` gets updated.
@@ -41,45 +74,50 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     // Info of each pool.
     struct PoolInfo {
         IERC20 lpToken;           // Address of LP token contract.
-        uint256 allocPoint;       // How many allocation points assigned to this pool. FHMs to distribute per block.
-        uint256 lastRewardBlock;  // Last block number that FHMs distribution occurs.
-        uint256 accFhmPerShare;   // Accumulated FHMs per share, times 1e12. See below.
+        uint allocPoint;       // How many allocation points assigned to this pool. FHMs to distribute per block.
+        uint lastRewardBlock;  // Last block number that FHMs distribution occurs.
+        uint accFhmPerShare;   // Accumulated FHMs per share, times 1e12. See below.
         uint16 depositFeeBP;      // Deposit fee in basis points
     }
 
     // The FHM TOKEN!
-    FantohmERC20Token public fhm;
+    IERC20 public fhm;
     // Dev address.
     address public devaddr;
     // LQDR tokens created per block.
-    uint256 public fhmPerBlock;
+    uint public fhmPerBlock;
     // Bonus muliplier for early FHM makers.
-    uint256 public constant BONUS_MULTIPLIER = 1;
+    uint public constant BONUS_MULTIPLIER = 1;
     // Deposit Fee address
     address public feeAddress;
+
+    address public immutable fhud; // FIXME init
+    address public immutable treasury; // FIXME init
+    address public immutable fhudMinter; // FIXME init
+    address public immutable balancerVault; // FIXME init
 
     // Info of each pool.
     PoolInfo[] public poolInfo;
     // Info of each user that stakes LP tokens.
-    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+    mapping(uint => mapping(address => UserInfo)) public userInfo;
     // Total allocation points. Must be the sum of all allocation points in all pools.
-    uint256 public totalAllocPoint = 0;
+    uint public totalAllocPoint = 0;
     // The block number when FHM mining starts.
-    uint256 public startBlock;
+    uint public startBlock;
 
-    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
-    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
-    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event Deposit(address indexed user, uint indexed pid, uint amount);
+    event Withdraw(address indexed user, uint indexed pid, uint amount);
+    event EmergencyWithdraw(address indexed user, uint indexed pid, uint amount);
     event SetFeeAddress(address indexed user, address indexed newAddress);
     event SetDevAddress(address indexed user, address indexed newAddress);
-    event UpdateEmissionRate(address indexed user, uint256 fhmPerBlock);
+    event UpdateEmissionRate(address indexed user, uint fhmPerBlock);
 
     constructor(
-        FantohmERC20Token _fhm,
+        IERC20 _fhm,
         address _devaddr,
         address _feeAddress,
-        uint256 _fhmPerBlock,
-        uint256 _startBlock
+        uint _fhmPerBlock,
+        uint _startBlock
     ) public {
         fhm = _fhm;
         devaddr = _devaddr;
@@ -88,7 +126,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         startBlock = _startBlock;
     }
 
-    function poolLength() external view returns (uint256) {
+    function poolLength() external view returns (uint) {
         return poolInfo.length;
     }
 
@@ -99,25 +137,25 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     }
 
     // Add a new lp to the pool. Can only be called by the owner.
-    function add(uint256 _allocPoint, IERC20 _lpToken, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner nonDuplicated(_lpToken) {
+    function add(uint _allocPoint, IERC20 _lpToken, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner nonDuplicated(_lpToken) {
         require(_depositFeeBP <= 10000, "add: invalid deposit fee basis points");
         if (_withUpdate) {
             massUpdatePools();
         }
-        uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        uint lastRewardBlock = block.number > startBlock ? block.number : startBlock;
         totalAllocPoint = totalAllocPoint.add(_allocPoint);
         poolExistence[_lpToken] = true;
         poolInfo.push(PoolInfo({
         lpToken : _lpToken,
         allocPoint : _allocPoint,
         lastRewardBlock : lastRewardBlock,
-        accLqdrPerShare : 0,
+        accFhmPerShare : 0,
         depositFeeBP : _depositFeeBP
         }));
     }
 
     // Update the given pool's LQDR allocation point and deposit fee. Can only be called by the owner.
-    function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
+    function set(uint _pid, uint _allocPoint, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
         require(_depositFeeBP <= 10000, "set: invalid deposit fee basis points");
         if (_withUpdate) {
             massUpdatePools();
@@ -128,45 +166,45 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     }
 
     // Return reward multiplier over the given _from to _to block.
-    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+    function getMultiplier(uint _from, uint _to) public view returns (uint) {
         return _to.sub(_from).mul(BONUS_MULTIPLIER);
     }
 
     // View function to see pending FHMs on frontend.
-    function pendingLqdr(uint256 _pid, address _user) external view returns (uint256) {
+    function pendingLqdr(uint _pid, address _user) external view returns (uint) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
-        uint256 accLqdrPerShare = pool.accFhmPerShare;
-        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        uint accFhmPerShare = pool.accFhmPerShare;
+        uint lpSupply = pool.lpToken.balanceOf(address(this));
         if (block.number > pool.lastRewardBlock && lpSupply != 0) {
-            uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 fhmReward = multiplier.mul(fhmPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-            accLqdrPerShare = accLqdrPerShare.add(fhmReward.mul(1e12).div(lpSupply));
+            uint multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+            uint fhmReward = multiplier.mul(fhmPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accFhmPerShare = accFhmPerShare.add(fhmReward.mul(1e12).div(lpSupply));
         }
-        return user.amount.mul(accLqdrPerShare).div(1e12).sub(user.rewardDebt);
+        return user.amount.mul(accFhmPerShare).div(1e12).sub(user.rewardDebt);
     }
 
     // Update reward variables for all pools. Be careful of gas spending!
     function massUpdatePools() public {
-        uint256 length = poolInfo.length;
-        for (uint256 pid = 0; pid < length; ++pid) {
+        uint length = poolInfo.length;
+        for (uint pid = 0; pid < length; ++pid) {
             updatePool(pid);
         }
     }
 
     // Update reward variables of the given pool to be up-to-date.
-    function updatePool(uint256 _pid) public {
+    function updatePool(uint _pid) public {
         PoolInfo storage pool = poolInfo[_pid];
         if (block.number <= pool.lastRewardBlock) {
             return;
         }
-        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        uint lpSupply = pool.lpToken.balanceOf(address(this));
         if (lpSupply == 0 || pool.allocPoint == 0) {
             pool.lastRewardBlock = block.number;
             return;
         }
-        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-        uint256 fhmReward = multiplier.mul(fhmPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        uint multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint fhmReward = multiplier.mul(fhmPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
         // FIMXE mint with treasury
         fhm.mint(devaddr, fhmReward.div(12));
         fhm.mint(address(this), fhmReward);
@@ -175,12 +213,12 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     }
 
     // Deposit LP tokens to MasterChef for FHM allocation.
-    function deposit(uint256 _pid, uint256 _amount) public nonReentrant {
+    function deposit(uint _pid, uint _amount) public nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accFhmPerShare).div(1e12).sub(user.rewardDebt);
+            uint pending = user.amount.mul(pool.accFhmPerShare).div(1e12).sub(user.rewardDebt);
             if (pending > 0) {
                 safeFhmTransfer(msg.sender, pending);
             }
@@ -188,7 +226,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         if (_amount > 0) {
             pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
             if (pool.depositFeeBP > 0) {
-                uint256 depositFee = _amount.mul(pool.depositFeeBP).div(10000);
+                uint depositFee = _amount.mul(pool.depositFeeBP).div(10000);
                 pool.lpToken.safeTransfer(feeAddress, depositFee);
                 user.amount = user.amount.add(_amount).sub(depositFee);
             } else {
@@ -200,12 +238,12 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     }
 
     // Withdraw LP tokens from MasterChef.
-    function withdraw(uint256 _pid, uint256 _amount) public nonReentrant {
+    function withdraw(uint _pid, uint _amount) public nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
-        uint256 pending = user.amount.mul(pool.accFhmPerShare).div(1e12).sub(user.rewardDebt);
+        uint pending = user.amount.mul(pool.accFhmPerShare).div(1e12).sub(user.rewardDebt);
         if (pending > 0) {
             safeFhmTransfer(msg.sender, pending);
         }
@@ -218,10 +256,10 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     }
 
     // Withdraw without caring about rewards. EMERGENCY ONLY.
-    function emergencyWithdraw(uint256 _pid) public nonReentrant {
+    function emergencyWithdraw(uint _pid) public nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
-        uint256 amount = user.amount;
+        uint amount = user.amount;
         user.amount = 0;
         user.rewardDebt = 0;
         pool.lpToken.safeTransfer(address(msg.sender), amount);
@@ -229,8 +267,8 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     }
 
     // Safe FHM transfer function, just in case if rounding error causes pool to not have enough FHMs.
-    function safeFhmTransfer(address _to, uint256 _amount) internal {
-        uint256 fhmBal = fhm.balanceOf(address(this));
+    function safeFhmTransfer(address _to, uint _amount) internal {
+        uint fhmBal = fhm.balanceOf(address(this));
         bool transferSuccess = false;
         if (_amount > fhmBal) {
             transferSuccess = fhm.transfer(_to, fhmBal);
@@ -254,9 +292,43 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     }
 
     //Pancake has to add hidden dummy pools inorder to alter the emission, here we make it simple and transparent to all.
-    function updateEmissionRate(uint256 _fhmPerBlock) public onlyOwner {
+    function updateEmissionRate(uint _fhmPerBlock) public onlyOwner {
         massUpdatePools();
         fhmPerBlock = _fhmPerBlock;
         emit UpdateEmissionRate(msg.sender, _fhmPerBlock);
+    }
+
+    function getMarketPrice() public view returns (uint _marketPrice) {
+        _marketPrice = IFHUDMinter(fhudMinter).getMarketPrice();
+    }
+
+    function joinPool(uint _pid, address _lpToken, address _principle, uint _amount) public nonReentrant returns (uint _lpTokenAmount) {
+        // FIXME transferFrom msg.sender amount of token to this contract, so its here
+        require(_amount > 0.01 ether, "MIN_TOKENS");
+
+        // FIXME fhud mint should go to the bond source code
+        uint fhmValue = _amount.mul(10**2).div(getMarketPrice());
+        ITreasury(treasury).mintRewards(address(this), fhmValue);
+        IMintable(fhud).mint(address(this), _amount);
+        IBurnable(fhm).burn(fhmValue);
+
+        IERC20(fhud).safeApprove(balancerVault, _amount);
+        IERC20(_principle).safeApprove(balancerVault, _amount);
+
+        uint[] calldata rawAmounts = [_amount, _amount];
+        bytes calldata userDataEncoded = abi.encode(['uint256', 'uint256[]'], [1 /* EXACT_TOKENS_IN_FOR_BPT_OUT */], rawAmounts);
+
+        JoinPoolRequest calldata request = JoinPoolRequest({
+            assets: [fhud, _principle],
+            maxAmountsIn: userDataEncoded,
+            userData: userDataEncoded,
+            fromInternalBalance: false
+        });
+
+        uint tokensBefore = IERC20(_lpToken).balanceOf(address(this));
+        IVault(balancerVault).joinPool(_pid, address(this), address(this), request);
+        uint tokensAfter = IERC20(_lpToken).balanceOf(address(this));
+
+        _lpTokenAmount = tokensAfter.sub(tokensBefore);
     }
 }
