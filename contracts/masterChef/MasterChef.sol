3db5106fe1196fed9f569faa3a69b2a909f2de4f
Chinu Dev <admin@chubbyinu.com>
2022-03-03 03:59:44 +0100

    calling for updated LP amount

--- contracts/masterChef/MasterChef.sol
+++ contracts/masterChef/MasterChef.sol
@@ -58,13 +58,14 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         //   3. User's `amount` gets updated.
         //   4. User's `rewardDebt` gets updated.
     }
-    // Info of each pool.
+    /// @notice Info of each pool.
     struct PoolInfo {
-        IERC20 lpToken;           // Address of LP token contract.
-        uint allocPoint;       // How many allocation points assigned to this pool. FHMs to distribute per block.
-        uint lastRewardBlock;  // Last block number that FHMs distribution occurs.
-        uint accFhmPerShare;   // Accumulated FHMs per share, times 1e12. See below.
-        uint16 depositFeeBP;      // Deposit fee in basis points
+        IERC20 lpToken;         // Address of LP token contract.
+        uint allocPoint;        // How many allocation points assigned to this pool. FHMs to distribute per block.
+        uint lastRewardBlock;   // Last block number that FHMs distribution occurs.
+        uint accFhmPerShare;    // Accumulated FHMs per share, times 1e12. See below.
+        uint16 depositFeeBP;    // Deposit fee in basis points
+        uint depositedTokens;   // total LP tokens deposited, so rest are fees which should distribute to each holder
     }
 
     // The FHM TOKEN!
@@ -118,7 +119,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
 
     mapping(IERC20 => bool) public poolExistence;
 
-    // Pool ID Tracker Mapper
+    /// @notice Pool ID Tracker Mapper
     mapping(IERC20 => uint256) public poolIdForLpAddress;
 
     function getPoolIdForLpToken(IERC20 _lpToken) external view returns (uint256) {
@@ -130,7 +131,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         _;
     }
 
-    // Add a new lp to the pool. Can only be called by the owner.
+    /// @notice Add a new lp to the pool. Can only be called by the owner.
     function add(uint _allocPoint, IERC20 _lpToken, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner nonDuplicated(_lpToken) {
         require(_depositFeeBP <= 10000, "add: invalid deposit fee basis points");
         if (_withUpdate) {
@@ -144,11 +145,12 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         allocPoint : _allocPoint,
         lastRewardBlock : lastRewardBlock,
         accFhmPerShare : 0,
-        depositFeeBP : _depositFeeBP
+        depositFeeBP : _depositFeeBP,
+        depositedTokens: 0
         }));
     }
 
-    // Update the given pool's Fhm allocation point and deposit fee. Can only be called by the owner.
+    /// @notice Update the given pool's Fhm allocation point and deposit fee. Can only be called by the owner.
     function set(uint _pid, uint _allocPoint, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
         require(_depositFeeBP <= 10000, "set: invalid deposit fee basis points");
         if (_withUpdate) {
@@ -158,12 +160,13 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         poolInfo[_pid].allocPoint = _allocPoint;
         poolInfo[_pid].depositFeeBP = _depositFeeBP;
     }
-    // Set the migrator contract. Can only be called by the owner.
+
+    /// @notice Set the migrator contract. Can only be called by the owner.
     function setMigrator(IMigratorChef _migrator) public onlyOwner {
         migrator = _migrator;
     }
 
-    // Migrate lp token to another lp contract. Can be called by anyone. We trust that migrator contract is good.
+    /// @notice Migrate lp token to another lp contract. Can be called by anyone. We trust that migrator contract is good.
     function migrate(uint256 _pid) public {
         require(address(migrator) != address(0), "migrate: no migrator");
         PoolInfo storage pool = poolInfo[_pid];
@@ -174,12 +177,13 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         require(bal == newLpToken.balanceOf(address(this)), "migrate: bad");
         pool.lpToken = newLpToken;
     }
-    // Return reward multiplier over the given _from to _to block.
+
+    /// @notice Return reward multiplier over the given _from to _to block.
     function getMultiplier(uint _from, uint _to) public pure returns (uint) {
         return _to.sub(_from).mul(BONUS_MULTIPLIER);
     }
 
-    // View function to see pending FHMs on frontend.
+    /// @notice View function to see pending FHMs on frontend.
     function pendingFhm(uint _pid, address _user) external view returns (uint) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
@@ -193,7 +197,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         return user.amount.mul(accFhmPerShare).div(1e12).sub(user.rewardDebt);
     }
 
-    // Update reward variables for all pools. Be careful of gas spending!
+    /// @notice Update reward variables for all pools. Be careful of gas spending!
     function massUpdatePools() public {
         uint length = poolInfo.length;
         for (uint pid = 0; pid < length; ++pid) {
@@ -201,7 +205,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         }
     }
 
-    // Update reward variables of the given pool to be up-to-date.
+    /// @notice Update reward variables of the given pool to be up-to-date.
     function updatePool(uint _pid) public {
         PoolInfo storage pool = poolInfo[_pid];
         if (block.number <= pool.lastRewardBlock) {
@@ -221,7 +225,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         pool.lastRewardBlock = block.number;
     }
 
-    // Deposit LP tokens to MasterChef for FHM allocation.
+    /// @notice Deposit LP tokens to MasterChef for FHM allocation.
     function deposit(uint _pid, uint _amount, address _claimable) public nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_claimable];
@@ -234,6 +238,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
             }
         }
         if (_amount > 0) {
+            pool.depositedTokens = pool.depositedTokens.add(_amount);
             pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
             if (pool.depositFeeBP > 0) {
                 uint depositFee = _amount.mul(pool.depositFeeBP).div(10000);
@@ -247,26 +252,47 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         emit Deposit(_claimable, _pid, _amount);
     }
 
-    // Withdraw LP tokens from MasterChef.
-    function withdraw(uint _pid, uint _amount, address _claimable) public nonReentrant {
+    /// @notice Withdraw LP tokens from MasterChef.
+    function withdraw(uint _pid, uint _minimalAmount, address _claimable) public nonReentrant {
         // FIXME need to rewrite logic to withdraw by _withdrawable
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_claimable];
-        require(user.amount >= _amount, "withdraw: not good");
+        require(user.amount >= _minimalAmount, "withdraw: not good");
         updatePool(_pid);
         uint pending = user.amount.mul(pool.accFhmPerShare).div(1e12).sub(user.rewardDebt);
         if (pending > 0) {
             safeFhmTransfer(_claimable, pending);
         }
-        if (_amount > 0) {
-            user.amount = user.amount.sub(_amount);
-            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        if (_minimalAmount > 0) {
+            user.amount = user.amount.sub(_minimalAmount);
+            uint transferableAmount = _minimalAmount.add(claimableFees(_pid, _minimalAmount));
+            pool.lpToken.safeTransfer(address(msg.sender), transferableAmount);
+
+            if (_minimalAmount < pool.depositedTokens) {
+                pool.depositedTokens = pool.depositedTokens.sub(_minimalAmount);
+            } else {
+                pool.depositedTokens = 0;
+            }
         }
         user.rewardDebt = user.amount.mul(pool.accFhmPerShare).div(1e12);
-        emit Withdraw(_claimable, _pid, _amount);
+        emit Withdraw(_claimable, _pid, _minimalAmount);
     }
 
-    //Claim fhm rewards
+    /// @notice shows how many LP tokens belongs to this staking TVL
+    /// @param _pid poolId
+    /// @param _amount amount
+    /// @return claimableAmount
+    function claimableFees(uint _pid, uint _amount) public view returns (uint) {
+        PoolInfo storage pool = poolInfo[_pid];
+        uint totalAmount = IERC20(pool.lpToken).balanceOf(address(this));
+        uint fees = 0;
+        if (totalAmount > pool.depositedTokens) {
+            fees = totalAmount.sub(pool.depositedTokens);
+        }
+        return fees.mul(pool.depositedTokens).div(_amount);
+    }
+
+    /// @notice Claim fhm rewards
     function harvest(uint256 _pid, address _to) public nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
@@ -287,9 +313,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         emit Harvest(msg.sender, _pid, eligibleFhm);
     }
 
-    // FIXME need to write logic how many LP tokens _user have
-
-    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    /// @notice Withdraw without caring about rewards. EMERGENCY ONLY.
     function emergencyWithdrawContract(uint _pid, address _user) public onlyOwner nonReentrant {
         require(_user.isContract(), "emergencyWithdrawContract: not contract");
         PoolInfo storage pool = poolInfo[_pid];
@@ -301,6 +325,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         emit EmergencyWithdraw(_user, _pid, amount);
     }
 
+    /// @notice Withdraw without caring about rewards. EMERGENCY ONLY.
     function emergencyWithdraw(uint _pid) public nonReentrant {
         // FIXME need to rewrite logic to withdraw by _withdrawable
         PoolInfo storage pool = poolInfo[_pid];
@@ -312,7 +337,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         emit EmergencyWithdraw(msg.sender, _pid, amount);
     }
 
-    // Safe FHM transfer function, just in case if rounding error causes pool to not have enough FHMs.
+    /// @notice Safe FHM transfer function, just in case if rounding error causes pool to not have enough FHMs.
     function safeFhmTransfer(address _to, uint _amount) internal {
         uint fhmBal = fhm.balanceOf(address(this));
         bool transferSuccess = false;
@@ -324,7 +349,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         require(transferSuccess, "safeFHMTransfer: transfer failed");
     }
 
-    // Update treasury address by the owner.
+    /// @notice Update treasury address by the owner.
     function treasury(address _treasuryAddress) public onlyOwner {
         treasuryAddress = _treasuryAddress;
         emit SetTreasuryAddress(treasuryAddress, _treasuryAddress);
@@ -334,7 +359,8 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         feeAddress = _feeAddress;
         emit SetFeeAddress(msg.sender, _feeAddress);
     }
-    //Pancake has to add hidden dummy pools inorder to alter the emission, here we make it simple and transparent to all.
+
+    /// @notice Pancake has to add hidden dummy pools inorder to alter the emission, here we make it simple and transparent to all.
     function updateEmissionRate(uint _fhmPerBlock) public onlyOwner {
         massUpdatePools();
         fhmPerBlock = _fhmPerBlock;
